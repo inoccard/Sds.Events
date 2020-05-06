59ecd06dd690f4fb2f88742bb949780288f5f166
Inocêncio <inocencio.cardoso@iatec.com>
2020-05-06 13:41:49 -0300

    binding e interpolação, e ngif

--- ProAgil-App/src/app/events/events.component.ts
+++ ProAgil-App/src/app/events/events.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventsComponent implements OnInit {
 
-  events: any;
+  events: any = [];
 
   constructor(private http: HttpClient) { }
 
