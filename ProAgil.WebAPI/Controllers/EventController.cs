b160ae9a662b6024f029ef435180ed434c4e44b9
Inocêncio <inocencio.cardoso@iatec.com>
2020-05-05 23:58:16 -0300

    Adicionado tratamento de exceções

--- ProAgil.WebAPI/Controllers/EventController.cs
+++ ProAgil.WebAPI/Controllers/EventController.cs
@@ -23,34 +23,15 @@ namespace ProAgil.WebAPI.Controllers {
 
         [HttpGet ("{id}")]
         public IActionResult Get (int id) {
-            var _event = new Event[] {
-                new Event {
-                    Id = 1,
-                        Theme = "Angular e .NET Core",
-                        Local = "Hortolandia",
-                        Lot = "1º Lote",
-                        PersonQtd = 250,
-                        EventDate = DateTime.Now.AddDays (2).ToString ("dd/MM/yyy")
-                },
-                new Event {
-                    Id = 2,
-                        Theme = "Angular",
-                        Local = "São Paulo",
-                        Lot = "2º Lote",
-                        PersonQtd = 350,
-                        EventDate = DateTime.Now.AddDays (2).ToString ("dd/MM/yyy")
-                },
-                new Event {
-                    Id = 3,
-                        Theme = ".NET Core",
-                        Local = "Campinas",
-                        Lot = "3º Lote",
-                        PersonQtd = 450,
-                        EventDate = DateTime.Now.AddDays (2).ToString ("dd/MM/yyy")
-                }
-            }.FirstOrDefault (a => a.Id == id);
-
+            try
+            {
+                var _event = context.Events.FirstOrDefault (a => a.Id == id);
             return Ok (_event);
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 }
