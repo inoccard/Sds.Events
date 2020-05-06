fe4bac4906e95774340ab816b10debd1dbcffc3b
Inocêncio <inocencio.cardoso@iatec.com>
2020-05-06 00:10:51 -0300

    modificado os métodos para assíncronos

--- ProAgil.WebAPI/Controllers/EventController.cs
+++ ProAgil.WebAPI/Controllers/EventController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProAgil.WebAPI.Data;
-using ProAgil.WebAPI.Models;
 
-namespace ProAgil.WebAPI.Controllers {
+namespace ProAgil.WebAPI.Controllers
+{
     [ApiController]
     [Route ("[controller]")]
     public class EventController : ControllerBase {
@@ -16,21 +15,22 @@ namespace ProAgil.WebAPI.Controllers {
         }
 
         [HttpGet]
-        public IEnumerable<Event> Get () {
-            var events = context.Events.ToList ();
-            return events;
+        public async Task<IActionResult> Get () {
+            try {
+                var events = await context.Events.ToListAsync ();
+                return Ok (events);
+            } catch (System.Exception) {
+                return this.StatusCode (StatusCodes.Status500InternalServerError, "Não é possível obtér a lista de eventos");
+            }
         }
 
         [HttpGet ("{id}")]
-        public IActionResult Get (int id) {
-            try
-            {
-                var _event = context.Events.FirstOrDefault (a => a.Id == id);
-            return Ok (_event);
-            }
-            catch (System.Exception e)
-            {
-                throw new Exception(e.Message);
+        public async Task<IActionResult> Get (int id) {
+            try {
+                var _event = await context.Events.FirstOrDefaultAsync (a => a.Id == id);
+                return Ok (_event);
+            } catch (System.Exception) {
+                return this.StatusCode (StatusCodes.Status500InternalServerError, "Não é possível obtér o evento");
             }
         }
     }
