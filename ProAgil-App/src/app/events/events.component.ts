d8ec3f0c7a5f81b2072bba0b771917de86440f55
Inocêncio <inocencio.cardoso@iatec.com>
2020-05-07 00:34:26 -0300

    Propriedade e Evento Binding

--- ProAgil-App/src/app/events/events.component.ts
+++ ProAgil-App/src/app/events/events.component.ts
@@ -9,11 +9,16 @@ import { HttpClient } from '@angular/common/http';
 export class EventsComponent implements OnInit {
 
   events: any = [];
+  showImg = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEvents();
+  }
+
+  alterImg() {
+    this.showImg = !this.showImg;
   }
 
   getEvents() {
