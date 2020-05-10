19d1f5b402dabe1b076ceeb03206ebab5d06fdfb
inoccard <inocencio.cardoso@iatec.com>
2020-05-10 05:32:21 -0300

    Adicionado modal template

--- ProAgil-App/src/app/events/events.component.ts
+++ ProAgil-App/src/app/events/events.component.ts
@@ -1,6 +1,6 @@
 import { EventService } from './../services/event/event.service';
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-events',
@@ -8,9 +8,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
+  eventsFiltered: any = [];
+  events: any = [];
+  showImg = false;
   // tslint:disable-next-line: variable-name
   _filterList: string;
-
+  modalRef: BsModalRef;
+  constructor(private eventService: EventService, private modalService: BsModalService) { }
+  ngOnInit() {
+    this.getEvents();
+  }
   get filterList(): string {
     return this._filterList;
   }
@@ -18,33 +25,29 @@ export class EventsComponent implements OnInit {
     this._filterList = value;
     this.eventsFiltered = this._filterList ? this.filterEvent(this._filterList) : this.events;
   }
-  eventsFiltered: any = [];
-  events: any = [];
-  showImg = false;
-
-  constructor(private eventService: EventService) { }
-  ngOnInit() {
-    this.getEvents();
+  // Abrir modal
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
-
   filterEvent(filterBy: string): any {
     filterBy = filterBy.toLocaleLowerCase();
     return this.events.filter(
       (      event: { theme: string; }) => event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1
     );
   }
-
+  //mostrar | ocultar imagem
   alterImg() {
     this.showImg = !this.showImg;
   }
+  // Obter eventos
   getEvents() {
     this.eventService.getEvents().subscribe(
-    response => {
+      response => {
         this.events = response;
         this.eventsFiltered = this.events;
-    }, error => {
-      console.log(error);
-    }
+      }, error => {
+        console.log(error);
+      }
     );
   }
 }
