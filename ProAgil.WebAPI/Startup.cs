93aa7dd28d97576d6d92988b421d969494139bc6
inoccard <inocenciocardoso19@gmail.com>
2020-07-24 17:02:31 -0300

    ...

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text;
 using AutoMapper;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -19,105 +21,122 @@ using ProAgil.Repository.Data;
 
 namespace ProAgil.WebAPI
 {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            /// <summary>
-            /// Injeção de Dependêncica
-            /// </summary>
-            /// <param name="("DefaultConnection""></param>
-            /// <typeparam name="DataContext"></typeparam>
-            /// <returns></returns>
-            //services.AddDbContext<Repository.Data.ProAgilContext> (d => d.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
-            /// <summary>
-            /// Injeção de Dependência do ProAgilRepository
-            /// </summary>
-            /// <typeparam name="IProAgilRepository"></typeparam>
-            /// <typeparam name="ProAgilRepository"></typeparam>
-            /// <returns></returns>
-            services.AddScoped<IProAgilRepository, ProAgilRepository> ();
-
-            // todos os controller terão que passar por uma autenticação
-            // quem vai consumir a API precisa estar autenticado e autorizado
-            // remove as obrigatoriedades padrão de senha
-            IdentityBuilder builder = services.AddIdentityCore<User> (options => 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            try
             {
-                options.Password.RequireDigit = false; // sem caracteres especiais
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 4;
-            });
+                /// <summary>
+                /// Injeção de Dependêncica
+                /// </summary>
+                /// <param name="("DefaultConnection""></param>
+                /// <typeparam name="ProAgilContext"></typeparam>
+                /// <returns></returns>
+                services.AddDbContext<ProAgilContext>(d => d.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                //services.AddDbContext<ProAgilContext> (d => d.UseSqlite (Configuration.GetConnectionString ("DefaultConnectionSqlite")));
 
-            /// <summary>
-            /// instancia o IdentityBuilder com o tipo de usuário, tippo de papel e o serviço criado acima
-            /// </summary>
-            /// <returns></returns>
-            builder = new IdentityBuilder (builder.UserType, typeof (Role), builder.Services);
-            // EntityFramework levará em consideração sempre o contexto
-            builder.AddEntityFrameworkStores<ProAgilContext> ();
-            builder.AddRoleValidator<Role> ();
-            builder.AddRoleManager<Role> (); // gerenciador dos papeis
-            builder.AddSignInManager<SignInManager<User>> ();
+                /// <summary>
+                /// Injeção de Dependência do ProAgilRepository
+                /// </summary>
+                /// <typeparam name="IProAgilRepository"></typeparam>
+                /// <typeparam name="ProAgilRepository"></typeparam>
+                /// <returns></returns>
+                services.AddScoped<IProAgilRepository, ProAgilRepository>();
 
-            // JWT Config
-            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(opt =>
+                // todos os controller terão que passar por uma autenticação
+                // quem vai consumir a API precisa estar autenticado e autorizado
+                // remove as obrigatoriedades padrão de senha
+                IdentityBuilder builder = services.AddIdentityCore<User>(options =>
                 {
-                    opt.TokenValidationParameters = new TokenValidationParameters
+                    options.Password.RequireDigit = false; // sem caracteres especiais
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 4;
+                })
+                    .AddEntityFrameworkStores<ProAgilContext>(); // EntityFramework levará em consideração sempre o contexto
+
+                /// <summary>
+                /// instancia o IdentityBuilder com o tipo de usuário, tippo de papel e o serviço criado acima
+                /// </summary>
+                /// <returns></returns>
+                builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+
+                //builder.AddRoleValidator<Role>();
+                builder.AddRoleManager<RoleManager<Role>>(); // gerenciador dos papeis
+                builder.AddSignInManager<SignInManager<User>>();
+
+                // JWT Config
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(opt =>
                     {
-                        // assinatura da chave do emissor
-                        ValidateIssuerSigningKey = true,
-                        // config da chave da API
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
+                        opt.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            // assinatura da chave do emissor
+                            ValidateIssuerSigningKey = true,
+                            // config da chave da API
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
 
-                    };
-                });
+                        };
+                    });
 
-            // Determina qual determinado controller será chamado, e adicionando uma política
-            // Não é mais necessário colocar autenticação no controller
-            services.AddMvc (options => {
+                // Determina qual determinado controller será chamado, e adicionando uma política
+                // Não é mais necessário colocar autenticação no controller
+                services.AddMvc(options =>
+                {
                     // toda vez que um controller for chamado, deverá respeitar esta política
-                    var policy = new AuthorizationPolicyBuilder ()
-                        .RequireAuthenticatedUser () // requer que o usuário esteja autenticado
-                        .Build ();
-                    options.Filters.Add (new AuthorizeFilter (policy)); // filtra todas as chamadas do controller
+                    var policy = new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser() // requer que o usuário esteja autenticado
+                        .Build();
+                    options.Filters.Add(new AuthorizeFilter(policy)); // filtra todas as chamadas do controller
 
                 })
-                .SetCompatibilityVersion (CompatibilityVersion.Version_3_0);
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-            services.AddAutoMapper ();
-            services.AddCors ();
-            //services.AddControllers();
+                services.AddAutoMapper();
+                services.AddCors();
+                services.AddControllers();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
-            app.UseHttpsRedirection ();
-            app.UseStaticFiles ();
-            app.UseStaticFiles (new StaticFileOptions () {
-                FileProvider = new PhysicalFileProvider (Path.Combine (Directory.GetCurrentDirectory (), @"Resources")),
-                    RequestPath = new PathString ("/Resources")
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+                RequestPath = new PathString("/Resources")
             });
-            app.UseRouting ();
+            app.UseRouting();
 
-            //app.UseAuthorization ();
+            app.UseAuthorization ();
 
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
             });
         }
     }
