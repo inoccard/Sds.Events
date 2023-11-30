15ac61025713150aa7c700dc57f620c249f62636
Inocêncio Cardoso <inocencio.cardoso@iatec.com>
2023-11-30 15:37:10 -0300

    corrigida importação de pacotes

--- ProAgil-App/src/app/services/auth.service.ts
+++ ProAgil-App/src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
 
-  baseURL = 'http://localhost:5000/api/user/';
+  baseURL = 'http://localhost:5000/api/v1/user/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
