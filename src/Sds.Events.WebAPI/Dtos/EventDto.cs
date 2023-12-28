2e230a090a01830a9bc3d2bdf6801b168e6375d0
inoccard <inocenciocardoso19@gmail.com>
2023-12-28 14:43:52 -0300

    feat: adicionado anotações de validação nas classes Dtos

--- src/Sds.Events.WebAPI/Dtos/EventDto.cs
+++ src/Sds.Events.WebAPI/Dtos/EventDto.cs
@@ -3,38 +3,41 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Sds.Events.WebAPI.Dtos
+namespace Sds.Events.WebAPI.Dtos;
+
+public class EventDto
 {
-    public class EventDto
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
-        [Column(TypeName = "varchar(500)")]
-        public string Local { get; set; }
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Column(TypeName = "varchar(500)")]
+    public string Local { get; set; }
 
-        [Required]
-        public DateTime EventDate { get; set; }
+    [Required(ErrorMessage = "Campo obrigatório")]
+    public DateTime EventDate { get; set; }
 
-        [Column(TypeName = "varchar(500)")]
-        [Required(ErrorMessage = "O tema deve ser Preenchido")]
-        public string Theme { get; set; }
+    [Column(TypeName = "varchar(500)")]
+    [Required(ErrorMessage = "O tema deve ser Preenchido")]
+    public string Theme { get; set; }
 
-        [Range(2, 12000, ErrorMessage = "Quantidade de pessoas deve ser entre 2 a 12000")]
-        public int PersonQtd { get; set; }
+    [Range(2, 12000, ErrorMessage = "Quantidade de pessoas deve ser entre 2 a 12000")]
+    public int PersonQtd { get; set; }
 
-        [Column(TypeName = "varchar(500)")]
-        public string ImageURL { get; set; }
+    [Column(TypeName = "varchar(500)")]
+    public string ImageURL { get; set; }
 
-        [Phone]
-        [Column(TypeName = "varchar(500)")]
-        public string ContactPhone { get; set; }
+    [Phone]
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Column(TypeName = "varchar(500)")]
+    public string ContactPhone { get; set; }
 
-        [Column(TypeName = "varchar(500)")]
-        public string ContactEmail { get; set; }
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Column(TypeName = "varchar(500)")]
+    [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+    [RegularExpression(@"^(?:(?!\buser@example\.com\b).)*$", ErrorMessage = "O endereço de e-mail deve ser diferente de 'user@example.com'")]
+    public string ContactEmail { get; set; }
 
-        public List<LotDto> Lots { get; set; }
-        public List<SocialNetworkDto> SocialNetworks { get; set; }
-        public List<SpeakerDto> Speakers { get; set; }
-    }
+    public List<LotDto> Lots { get; set; }
+    public List<SocialNetworkDto> SocialNetworks { get; set; }
+    public List<SpeakerDto> Speakers { get; set; }
 }
--- src/Sds.Events.WebAPI/Dtos/UserDto.cs
+++ src/Sds.Events.WebAPI/Dtos/UserDto.cs
@@ -1,10 +1,32 @@
-﻿namespace Sds.Events.WebAPI.Dtos
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sds.Events.WebAPI.Dtos
 {
     public class UserDto
     {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O e-mail deve ter entre 5 e 50 caracteres")]
+        [RegularExpression(@"^[a-zA-Z0-9_]*$", ErrorMessage = "O nome de usuário pode conter apenas letras, números e underscores.")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Column(TypeName = "varchar(500)")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+        [StringLength(500, MinimumLength = 5, ErrorMessage = "O e-mail deve ter entre 5 e 500 caracteres")]
+        [RegularExpression(@"^(?:(?!\buser@example\.com\b).)*$", ErrorMessage = "O endereço de e-mail deve ser diferente de 'user@example.com'")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Column(TypeName = "varchar(500)")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", 
+            ErrorMessage = "A senha deve ter pelo menos 5 caracteres, incluindo letras, números e caracteres especiais.")]
         public string PasswordHash { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Column(TypeName = "varchar(500)")]
         public string FullName { get; set; }
     }
 }
